cc41d052f53bd90284f895e0aa7161a91ad72d07
dazhige666 <dazhige666@outlook.com>
2024-06-12 14:13:09 +0800

    second

--- src/main/java/com/neuedu/boot002/Boot002Application.java
+++ src/main/java/com/neuedu/boot002/Boot002Application.java
@@ -1,9 +1,13 @@
 package com.neuedu.boot002;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//程序入口
+@MapperScan("com.neuedu.boot002.dao")
+//指定需要生成成实现类的接口所在的包
 public class Boot002Application {
 
     public static void main(String[] args) {
